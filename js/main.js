b9cd6dfa70f3d9a8986d22ddb76c97d912d3074a
Dacn0210 <141756428+Dacn0210@users.noreply.github.com>
2023-09-13 06:21:32 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// link github:https://github.com/Dacn0210
+// link linkedin: www.linkedin.com/in/diego-alejandro-castillo-niño-8b4a7a290
+function descargacv(){
+    window.location.href='doc/HV -Diego_Castillo.pdf';
+}
+
+var imagen1 = document.getElementById("github");
+imagen1.addEventListener("click", function() {
+    window.location.href = "https://github.com/Dacn0210";
+});
+
+var imagen2 = document.getElementById("linkedin");
+imagen2.addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/diego-alejandro-castillo-niño-8b4a7a290/ ";
+});
